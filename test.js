b5f128911377dd403e18bd3a496684c32d109733
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-29 11:09:04 +0800

    feat: add test file

---
+++ test.js
@@ -0,0 +1,11 @@
+import fs from "fs";
+import { remark } from "remark";
+import { remarkIndexedBlock } from "./build";
+
+const buffer = fs.readFileSync("example.md");
+
+const main = async () => {
+  const file = await remark().use(remarkIndexedBlock).process(buffer);
+};
+
+main();
