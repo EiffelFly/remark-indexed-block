aa70533e240dec27388f5827f0d378c9a7ca53a3
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-14 11:53:28 +0800

    feat: move folder

---
+++ packages/core/lib/index.ts
@@ -0,0 +1,10 @@
+import { remarkIndexedBlock } from "./remarkIndexedBlock";
+import { rehypeIndexedBlock } from "./rehypeIndexedBlock";
+import { indexedBlock, indexedBlockChildren } from "./rehypeHandler";
+
+export {
+  remarkIndexedBlock,
+  rehypeIndexedBlock,
+  indexedBlock,
+  indexedBlockChildren,
+};
